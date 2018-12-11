3fc901385d6a59deff129e06dedb18a64914796d
unknown <ssvrdlin@spartaglobal.com>
2018-12-11 23:30:43 +0000

    Added BMI function and error handling loop

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var calcVersion;
 
 do {
-  calcVersion = prompt("Which calculator do you wish to use? (1)Basic, (2)Advanced, (3)BMI, (4)Trip Calculator? Type 1, 2, 3, or 4");
-  if (isNaN(calcVersion) || calcVersion < 0 || calcVersion > 4) {
+  calcVersion = prompt("Which calculator do you wish to use? (1)Basic, (2)Advanced, (3)BMI? Type 1, 2, or 3");
+  if (isNaN(calcVersion) || calcVersion < 0 || calcVersion > 3) {
     prompt("Invalid input! Try again!");
   }
-} while (isNaN(calcVersion) || !(calcVersion < 5) || !(calcVersion > 0));
+} while (isNaN(calcVersion) || !(calcVersion < 4) || !(calcVersion > 0));
 
 if (calcVersion == 1) {
 
@@ -70,6 +70,25 @@ if (calcVersion == 1) {
     var result = Math.sqrt(parseInt(valueOne));
     prompt("Square root of " + valueOne + " = " + result);
   }
+} else {
+  var valueOne;
+  var valueTwo;
+
+  do {
+    valueOne = prompt("Please enter weight!");
+    if (isNaN(valueOne)) {
+      prompt("Invalid input! Try again!");
+    }
+  } while(isNaN(valueOne));
+
+  do {
+    valueTwo = prompt("Please enter height!");
+    if (isNaN(valueTwo)) {
+      prompt("Invalid input! Try again!");
+    }
+  } while(isNaN(valueTwo));
+
+  prompt("Your BMI value is: " + bmi(valueOne, valueTwo));
 }
 
 
@@ -81,45 +100,3 @@ function power(number1, number2) {
 function bmi(weight, height) {
   return weight / (height * height);
 }
-
-//
-// } else if (calcVersion == 2) {
-//   var operation = prompt("Please enter an operation to use: (1)Power, (2)SquareRoot. Type 1 or 2");
-//   var valueOne = prompt("Please enter a number!");
-//   var result;
-//
-//   if (operation == "1") {
-//     var valueTwo = prompt("Please enter a second number!");
-//     result = parseInt(valueOne) ** parseInt(valueTwo);
-//     console.log(valueOne + "^" + valueTwo + " = " + result);
-//   } else {
-//     result = Math.sqrt(parseInt(valueOne));
-//     console.log("Square root of " + valueOne + " = " + result);
-//   }
-// } else if (calcVersion == 3) {
-//   var metricOrImperial = prompt("Calculate using (1)Metric or (2)Imperial? Type 1 or 2");
-//   var weight = prompt("What is your weight?");
-//   var height = prompt("What is your height?")
-//   var bmi = weight/(height * height);
-//   console.log("Test: " + bmi);
-//
-//   if (metricOrImperial == 2) {
-//     bmi *= 703;
-//     console.log("Your bmi is: " + bmi);
-//   } else {
-//     console.log("Your bmi is: " + bmi);
-//   }
-//
-// } else {
-//   console.log("This functionality is not completed");
-//   // console.log("Lets plan a trip!");
-//   // var distance = prompt("How far is your trip? Enter a number in miles");
-//   // var fuelEfficiency = prompt("What is your fuel efficiency in MPG?");
-//   // var costPerGal = prompt("What is the cost per gallon of petrol?");
-//   // var speed = prompt("What speed will you be going at in MPH?");
-//   //
-//   // var time = distance / speed;
-//   // var overSixtyMph = speed - 60;
-//   // var reducedMpg = overSixtyMph * 2;
-//   // var actualFuelEfficiency = fuelEfficiency - reducedMpg;
-// }
